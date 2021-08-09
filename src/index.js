08530ba25030fcfe54907b30e118010b57da7eae
Litvinenko Boris <boris12lit@gmail.com>
2021-08-09 03:43:47 +0300

    Refactor state and func to OOP store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from "./redux/state";
-import {rerenderEntireTree} from "./render";
+import store from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 
-// eslint-disable-next-line no-undef
-rerenderEntireTree(state);
+// addPost();
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     addLetter={store.addLetter.bind(store)}/>
+            </React.StrictMode>
+        </BrowserRouter>, document.getElementById('root')
+    );
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,49 +1,61 @@
-import {rerenderEntireTree} from "../render";
+let store = {
+    _state: {
+        letterL: '',
+        profilePage: {
+            postsData: [
+                {id: 1, message: "Hi, how are you, my darling Props?", likesCount: 23},
+                {id: 2, message: "Hi, i'm fine", likesCount: 36},
+                {id: 3, message: "I'm fine, thx and u?", likesCount: 11},
+                {id: 4, message: "o la la", likesCount: 55}
+            ]
+        },
+        messagesPage: {
+            dialogsData: [
+                {id: 1, name: 'Lit_o'},
+                {id: 2, name: 'Dude'},
+                {id: 3, name: 'SpiderMan'},
+                {id: 4, name: 'SpiderLady'}
+            ],
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your hommies?43'},
+                {id: 3, message: "I'm fine, thx and u?"},
+                {id: 4, message: "I'm fine, thx and Yo?"}
+            ]
+        },
+        sideBar: {
+            friendsHot: ['Jhon', 'Jackie Chan', 'Fiona']
+        },
+    },
+    getState () {
+        return this._state;
+    },
+    addPost (){
+        let newPost = {
+            id: 5,
+            message: this._state.letterL,
+            likesCount: 0
+        };
+        this._state.profilePage.postsData.push(newPost);
+        this._state.letterL = '';
+        this._callSubsriber(this._state);
+    },
+    addLetter (letter) {
+        this._state.letterL = letter;
+        this._callSubsriber(this._state);
+    },
+    // сюда мы присвоим то, что придет в observer, тк, в const subscribe rerenderEntireTree не определен
+// и функция начнет искать объявленную переменную глобально в файле, но не в других функциях
+// она найдет let rerenderEntireTree и присвоет ему observer вместо console
+    _callSubsriber () {
+        console.log('state changed')
+    },
 
-let state = {
-    letterL: '',
-    profilePage: {
-        postsData: [
-            {id: 1, message: "Hi, how are you, my darling Props?", likesCount: 23},
-            {id: 2, message: "Hi, i'm fine", likesCount: 36},
-            {id: 3, message: "I'm fine, thx and u?", likesCount: 11},
-            {id: 4, message: "o la la", likesCount: 55}
-        ]
-    },
-    messagesPage: {
-        dialogsData: [
-            {id: 1, name: 'Lit_o'},
-            {id: 2, name: 'Dude'},
-            {id: 3, name: 'SpiderMan'},
-            {id: 4, name: 'SpiderLady'}
-        ],
-        messagesData: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your hommies?43'},
-            {id: 3, message: "I'm fine, thx and u?"},
-            {id: 4, message: "I'm fine, thx and Yo?"}
-        ]
-    },
-    sideBar: {
-        friendsHot: ['Jhon', 'Jackie Chan', 'Fiona']
-    },
-
+    subscribe (observer) {
+        this._callSubsriber = observer;
+        // Это паттерн observer похож на publisher-subscriber addEventListener
+    }
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.letterL,
-        likesCount: 0
-    };
-    state.profilePage.postsData.push(newPost);
-    state.letterL = '';
-    rerenderEntireTree(state);
-}
-
-export let addLetter = (letter) => {
-    state.letterL = letter;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
